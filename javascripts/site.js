5b1c02e3a013e1c2c5485e390f9004505803aeb7
Robert Ferney <rob@ferney.org>
2025-10-23 21:55:30 -0600

    Site updated to b573e8f

--- javascripts/site.js
+++ javascripts/site.js
@@ -1,1 +1,9 @@
-// This is where it all goes :)
+// site.js.erb
+import { Application } from "./stimulus.js"
+window.Stimulus = Application.start()
+
+import hello_controller from "./hello_controller.js";
+Stimulus.register("hello", hello_controller);
+import scale_controller from "./scale_controller.js";
+Stimulus.register("scale", scale_controller);
+
